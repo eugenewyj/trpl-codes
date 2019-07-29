0251d17fe5a5a50b9c9c849dfc801a79e6effe33
Eugene Wang <eugenewyj79@gmail.com>
2019-07-29 17:40:30 +0800

    添加示例代码

--- owership/src/main.rs
+++ owership/src/main.rs
@@ -1,11 +1,48 @@
 fn main() {
-    let s1 = String::from("hello");
+    let mut s1 = String::from("hello");
 
-    let len = calculate_length(&s1);
+    let len = calculate_length(&mut s1);
 
-    println!("The length of {} is {}.", s1, len)
+    println!("The length of {} is {}.", s1, len);
+
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    println!("word is {}", word);
+
+    let hello = first_word_str(&s);
+
+    println!("hello is {}", hello);
+
+    s.clear();
 }
 
-fn calculate_length(s: &String) -> usize {
+fn calculate_length(s: &mut String) -> usize {
+    s.push_str("aa");
     return s.len();
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+fn first_word_str(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
