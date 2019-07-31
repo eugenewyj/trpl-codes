d11ed2e4536370835c3e2348c7eb9b7adf6730ee
Eugene Wang <eugenewyj79@gmail.com>
2019-07-31 16:14:51 +0800

    添加struct示例代码

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,12 +8,22 @@ fn main() {
     println!("2:长方形面积是：{}", area_tuple(rect1));
 
     let rect2 = Rectangle{width: 30, height: 50};
+    println!("矩形是： {:?}", rect2);
+    println!("矩形是： {:#?}", rect2);
     println!("3:长方形面积是：{}", area_struct(&rect2));
+    println!("4:长方形面积是：{}", rect2.area1());
 }
 
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area1(&self) -> u32 {
+        self.width * self.height
+    }
 }
 
 fn area(width: u32, height: u32) -> u32 {
