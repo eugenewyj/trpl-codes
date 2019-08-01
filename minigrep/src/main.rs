da0c637d5312ce45bbaeecaacf6b65358b7b8b02
Eugene Wang <eugenewyj79@gmail.com>
2019-08-01 18:50:39 +0800

    add mingrep

---
+++ minigrep/src/main.rs
@@ -0,0 +1,27 @@
+use std::env;
+use std::process;
+
+use minigrep;
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments:{}", err);
+        process::exit(1);
+    });
+    
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
+
+// fn parese_config(args: &[String]) -> Config {
+//     let query = args[1].clone();
+//     let filename = args[2].clone();
+
+//     Config { query, filename }
+// }
